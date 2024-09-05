ed93aa3e9b1ae1cf31f7eb681370a43ac929d166
Eoic <karolis.strazdas@protonmail.com>
2024-09-06 01:46:59 +0300

    Setup React and add example Material UI dashboard.

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,28 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-    eslint.configs.recommended,
-    ...tseslint.configs.recommendedTypeChecked,
+    { ignores: ['dist'] },
     {
-        files: ['**/*.ts'],
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ['**/*.{ts,tsx}'],
         languageOptions: {
-            parserOptions: {
-                project: './tsconfig.eslint.json',
-                tsconfigRootDir: import.meta.dirname,
-            },
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
         },
         rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
             'no-unused-vars': 'off',
             curly: ['error', 'multi'],
             quotes: ['error', 'single'],
@@ -41,4 +51,47 @@ export default tseslint.config(
         files: ['**/*.js'],
         ...tseslint.configs.disableTypeChecked,
     },
-);
+)
+
+
+// export default tseslint.config(
+//     eslint.configs.recommended,
+//     ...tseslint.configs.recommendedTypeChecked,
+//     {
+//         files: ['**/*.{ts,tsx}'],
+//         languageOptions: {
+//             parserOptions: {
+//                 project: './tsconfig.eslint.json',
+//                 tsconfigRootDir: import.meta.dirname,
+//             },
+//         },
+//         rules: {
+//             'no-unused-vars': 'off',
+//             curly: ['error', 'multi'],
+//             quotes: ['error', 'single'],
+//             indent: ['error', 4],
+//             'max-len': ['error', 120],
+//             'semi': ['error', 'always'],
+//             'eol-last': ['error', 'always'],
+//             'comma-dangle': ["error", {
+//                 "arrays": "never",
+//                 "objects": "always",
+//                 "imports": "never",
+//                 "exports": "never",
+//                 "functions": "never"
+//             }],
+//             '@typescript-eslint/no-unused-vars': [
+//                 'warn',
+//                 {
+//                     argsIgnorePattern: '^_',
+//                     varsIgnorePattern: '^_',
+//                     caughtErrorsIgnorePattern: '^_',
+//                 },
+//             ],
+//         },
+//     },
+//     {
+//         files: ['**/*.js'],
+//         ...tseslint.configs.disableTypeChecked,
+//     },
+// );
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     base: '',
     server: {
         port: 9000
     },
+    plugins: [react()],
 });
